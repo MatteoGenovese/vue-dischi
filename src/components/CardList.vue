eb1d22b2ee2dd5293fef44e10ce09515309371eb
MatteoGenovese <matteo.genovese@icloud.com>
2022-07-20 16:29:22 +0200

    feat: add a select tag

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,18 +1,25 @@
 <template>
-  <div>
-
-    <div class="container d-flex flex-wrap p-5 card-container">
-      <Card v-for="(card,index) in cards"
-      :key="index"
-      :cPoster="card.poster"
-      :cTitle="card.title"
-      :cAuthor="card.author"
-      :cGenre="card.genre"
-      :cYear="card.year"
-      />
+  <section>
+    <div class="container">
+      <label for="genre">Choose genre:</label>
+      <select name="genre" id="genre">
+        <option value="" >All</option>
+        <option value="Rock" >Rock</option>
+        <option value="Pop" >Pop</option>
+        <option value="Jazz" >Jazz</option>
+        <option value="Metal" >Metal</option>
+      </select>
     </div>
 
-  </div>
+
+
+    <div class="container d-flex flex-wrap p-5 card-container">
+      <Card v-for="(card, index) in cards" :key="index" :cPoster="card.poster" :cTitle="card.title"
+        :cAuthor="card.author" :cGenre="card.genre" :cYear="card.year" />
+    </div>
+
+
+  </section>
 </template>
 
 <script>
@@ -36,32 +43,33 @@ export default {
 
 
       axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-      .then((result)=> {
-        console.log(result.data.response);
-        this.cards=result.data.response;
-      })
-      .catch((error) => {
-        console.warn(error);
-      })
+        .then((result) => {
+          console.log(result.data.response);
+          this.cards = result.data.response;
+        })
+        .catch((error) => {
+          console.warn(error);
+        })
 
 
 
     }
   }
   ,
-    created() {
-      this.getCard();
-    }
+  created() {
+    this.getCard();
+  }
 }
 </script>
 
 
 <style scoped lang="scss">
-
-.card-container{
+.card-container {
   row-gap: 14px;
   gap: 15px;
 }
-
+label{
+  color: white;
+}
 </style>
 
