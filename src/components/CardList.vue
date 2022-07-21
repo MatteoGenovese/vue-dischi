a90e0daa7f3cee669cbbe70f29513e3cbec473d8
MatteoGenovese <matteo.genovese@icloud.com>
2022-07-21 12:26:43 +0200

    feat: genre options inserted dinamically

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -2,17 +2,17 @@
   <section>
 
     <div class="container">
-      <GenreSelection @search="searchAlbum" />
+      <GenreSelection @search="searchAlbum" :genreList="genreList"/>
+
+      <!-- v-for="(genre,index) in genreList"
+      :key="index"
+      :genre="genre" -->
     </div>
 
 
 
     <div class="container d-flex flex-wrap p-5 card-container">
-      <Card
-        v-for="(card, index) in cardsFiltered"
-        :key="index"
-        :card="card"
-        />
+      <Card v-for="(card, index) in cardsFiltered" :key="index" :card="card" />
     </div>
 
 
@@ -34,40 +34,51 @@ export default {
   },
   data: function () {
     return {
+      dataStoraged: false,
       cards: [],
-      cardsFiltered:[],
+      cardsFiltered: [],
+      genreList: [],
+
     }
   },
   methods: {
-    getCard() {
-
+    getCards() {
       axios.get('https://flynn.boolean.careers/exercises/api/array/music')
         .then((result) => {
           console.log(result.data.response);
           this.cards = result.data.response;
-          this.cardsFiltered = result.data.response; //
-
+          this.cardsFiltered = result.data.response; //agisco sempre sull'array copiato per la visualizzazione
+          this.dataStoraged == true;
+          this.searchGenreToDisplay();
         })
         .catch((error) => {
           console.warn(error);
         })
     },
-    searchAlbum(needle){
-      if (needle=='All'){
+    searchAlbum(needle) {
+      if (needle == 'All') {
         this.cardsFiltered = [...this.cards]
       }
-      else{
-        console.log(needle)
-        this.cardsFiltered = this.cards.filter( (element) => {
-        console.log(element.genre, needle)
-        return element.genre == needle});
+      else {
+        this.cardsFiltered = this.cards.filter((element) => {
+          return element.genre == needle
+        });
       }
-      
-}
+    },
+    searchGenreToDisplay(){
+      this.genreList.push('All')
+        this.cards.forEach(element => {
+          if(!(this.genreList.includes(element.genre)))
+            this.genreList.push(element.genre)
+        });
+
+        console.log(this.genreList)
+    }
   }
   ,
   created() {
-    this.getCard();
+    this.getCards();
+
   }
 }
 </script>
@@ -78,6 +89,5 @@ export default {
   row-gap: 14px;
   gap: 15px;
 }
-
 </style>
 
--- src/components/GenreSelection.vue
+++ src/components/GenreSelection.vue
@@ -3,12 +3,12 @@
     <div>
         <label for="genre">Choose genre:</label>
         <select name="genre" id="genre" v-model="inputSelect" @change="$emit('search', inputSelect)">
-            <option value="All">All</option>
-            <option value="Rock">Rock</option>
-            <option value="Pop">Pop</option>
-            <option value="Jazz">Jazz</option>
-            <option value="Metal">Metal</option>
-        </select>  
+        <option
+        v-for="(genre,index) in genreList"
+        :key="index"
+        :value="genre">{{genre}}</option>
+
+        </select>
 
     </div>
 
@@ -20,15 +20,17 @@ export default {
     data: function () {
         return {
             inputSelect: "",
-            
+
         }
-    }
-    ,
+    },
     methods: {
         prova(needle) {
             console.log(inputSelect)
         }
-    }
+    },
+    props: {
+        'genreList':Array,
+    },
 
 }
 </script>
